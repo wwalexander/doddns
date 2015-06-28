4eafe48bf8cb9b4d91a2c67cd3420fe6724d9934
Will Alexander <github.com@willalexander.me>
2015-06-28 18:16:48 -0400

    Add client

--- README.md
+++ README.md
@@ -11,6 +11,30 @@ The DigitalOcean API allows domain name records to be updated. This means that i
 Usage
 -----
 
+### Server
+
 doduc has a server, which runs on your DigitalOcean droplet (or any other remote server). The server gets the source IP from the incoming HTTP requests and returns it to the client. It runs on port 18768. Thus, to run the server in the background on your machine, you would run:
 
+`doduc-server`
+
+If you wanted to run the server in the background so that you could exit the shell, you could run:
+
 `doduc-server &`
+
+or if you wanted to view any errors which caused the server to quiet:
+
+`nohup doduc-server &`
+
+which would save the output to a file in the same directory.
+
+### Client
+
+If you wish to use another server to get your external IP, the only requirement is that it must respond to a GET HTTP request with a valid IP address.
+
+To run the client, you must first [generate an OAuth token](https://cloud.digitalocean.com/settings/tokens/new) for the client to use. Save the generated token to a file (e.g. `token` in the root of this repository).
+
+Now you can run the client by specifying the DigitalOcean domain you want to update, the subdomain you wish to point to your IP address, the URL of your IP server, and the path to the file containing your OAuth token. For instance, if I wanted `home.mywebsite.com` to point to my IP address, I had `doduc-server` running on `www.mywebsite.com`, and I had my OAuth token saved in `token` in the current directory, I would run the client on my machine as follows:
+
+`doduc-client -domain mywebsite.com -subdomain home -ip_server http://www.mywebsite.com:18768 -token token`
+
+Similarly to the server, the client can be run in the background or with `nohup`.
---
+++ doduc-client/client.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"flag"
+	"github.com/digitalocean/godo"
+	"golang.org/x/oauth2"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type tokenSource struct {
+	accessToken string
+}
+
+func requireFlags(flags ...*string) {
+	for _, flag := range flags {
+		if *flag == "" {
+			log.Fatal("missing required flag")
+		}
+	}
+}
+
+func (ts tokenSource) Token() (*oauth2.Token, error) {
+	return &oauth2.Token{AccessToken: ts.accessToken}, nil
+}
+
+func update(domain string, subdomain string, ipServer string, client *godo.Client) {
+	drs, _, err := client.Domains.Records(domain, nil)
+	if err != nil {
+		log.Printf("unable to fetch domain records: %v", err)
+		return
+	}
+	var id *int
+	var addr string
+	for _, dr := range drs {
+		if dr.Type == "A" && dr.Name == subdomain {
+			id = &dr.ID
+			addr = dr.Data
+		}
+	}
+	resp, err := http.Get(ipServer)
+	if err != nil {
+		log.Println("unable to open request to IP server: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("unable to read response from IP server: %v", err)
+	}
+	newAddr := strings.TrimRight(string(body), "\n")
+	drer := &godo.DomainRecordEditRequest{
+		Type: "A",
+		Name: subdomain,
+		Data: newAddr,
+	}
+	if id == nil {
+		log.Println("record not found; creating new record")
+		_, _, err := client.Domains.CreateRecord(domain, drer)
+		if err != nil {
+			log.Println("unable to create new record: %v", err)
+			return
+		}
+	} else if newAddr != addr {
+		log.Println("address changed; updating record")
+		_, _, err := client.Domains.EditRecord(domain, *id, drer)
+		if err != nil {
+			log.Println("unable to update record: %v", err)
+			return
+		}
+	} else {
+		log.Println("record is up to date")
+	}
+}
+
+func main() {
+	flagDomain := flag.String("domain", "", "the DigitalOcean domain you want to update")
+	flagSubdomain := flag.String("subdomain", "", "the subdomain that should point to your IP address")
+	flagIPServer := flag.String("ip_server", "", "the doduc server")
+	flagToken := flag.String("token", "", "the file containing your OAuth2 token")
+	flag.Parse()
+	requireFlags(flagDomain, flagSubdomain, flagIPServer, flagToken)
+	token, err := ioutil.ReadFile(*flagToken)
+	if err != nil {
+		log.Fatal("unable to read token file '%s': %v", err)
+	}
+	ts := tokenSource{accessToken: string(token)}
+	client := godo.NewClient(oauth2.NewClient(oauth2.NoContext, ts))
+	for {
+		update(*flagDomain, *flagSubdomain, *flagIPServer, client)
+		time.Sleep(5 * time.Minute)
+	}
+}
