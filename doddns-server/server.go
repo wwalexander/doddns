ea3395f51f34f07677b72526e4b06c1fd22badbc
Will Alexander <github.com@willalexander.me>
2015-08-24 11:58:08 -0400

    Add TLS flags

--- doddns-server/server.go
+++ doddns-server/server.go
@@ -11,6 +11,12 @@ import (
 	"strings"
 )
 
+const (
+	fportName = "port"
+	fcertName = "cert"
+	fkeyName  = "key"
+)
+
 // Handler responds a request with its IP address.
 func Handler(w http.ResponseWriter, r *http.Request) {
 	host, _, err := net.SplitHostPort(r.RemoteAddr)
@@ -21,9 +27,30 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, host)
 }
 
+func flagIsSet(name string) bool {
+	set := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			set = true
+		}
+	})
+	return set
+}
+
 func main() {
+	fcert := flag.String(fcertName, "", "the TLS certificate to use")
+	fkey := flag.String(fkeyName, "", "the TLS key to use")
 	fport := flag.Uint("port", 18768, "the port to listen on")
 	flag.Parse()
+	fcertSet := flagIsSet(fcertName)
+	fkeySet := flagIsSet(fkeyName)
+	if fcertSet && !fkeySet {
+		log.Fatalf("%s set without %s", fcertName, fkeyName)
+	}
+	if !fcertSet && fkeySet {
+		log.Fatalf("%s set without %s", fkeyName, fcertName)
+	}
+	tls := fcertSet && fkeySet
 	logFile, err := os.OpenFile(filepath.Base(strings.TrimSuffix(os.Args[0],
 				filepath.Ext(os.Args[0])))+".log",
 		os.O_APPEND|os.O_CREATE, 0666)
@@ -32,5 +59,11 @@ func main() {
 	}
 	log.SetOutput(logFile)
 	http.HandleFunc("/", Handler)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *fport), nil))
+	addr := fmt.Sprintf(":%d", *fport)
+	if tls {
+		err = http.ListenAndServeTLS(addr, *fcert, *fkey, nil)
+	} else {
+		err = http.ListenAndServe(addr, nil)
+	}
+	log.Fatal(err)
 }
